7b43413494cf14c175d59ed1aebc8723d584677e
Deniss <thewwworm@gmail.com>
2018-07-20 07:02:49 +0300

    project files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+console.log('App Started');
+
+function copy(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);  
+}
+
+copy('42');
